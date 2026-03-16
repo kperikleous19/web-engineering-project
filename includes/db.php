77c401494d92f04d1355e2d4df3488f939ee2497
antrigeo <andri24005@gmail.com>
2026-03-16 10:49:32 +0200

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,20 @@
+<?php
 
+$host = "localhost";
+$dbname = "tepak_ee";
+$username = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch(PDOException $e){
+
+    die("Database connection failed");
+
+}
